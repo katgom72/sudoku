9c03216de71d1af7244d174cb8c10fb1433f889d
katgom72 <kasiag11122001@gmail.com>
2024-11-08 15:01:45 +0100

    Update registration panel design and add 'Login' button.

--- src/UserRegistrationScreen.java
+++ src/UserRegistrationScreen.java
@@ -11,19 +11,21 @@ import javax.imageio.ImageIO;
 import javax.swing.text.AttributeSet;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import org.json.JSONObject;
+import java.io.FileWriter;
+
+
+
 
 
 public class UserRegistrationScreen extends JFrame {
 
     private JTextField usernameField;
-    private JTextField ageField;
     private JTextField pinField;
 
     private JLabel error1Label;
     private JLabel error2Label;
-    private JLabel error3Label;
-    private JLabel error4Label; 
-
+    private JLabel error4Label;
 
     private ButtonGroup skillLevelGroup;
 
@@ -42,8 +44,6 @@ public class UserRegistrationScreen extends JFrame {
 
         usernameField = createRoundedTextField(260);
         pinField = createPasswordField(340);
-        ageField = createRoundedTextField(420);
-        ((PlainDocument) ageField.getDocument()).setDocumentFilter(new NumberOnlyFilter());
         ((PlainDocument) pinField.getDocument()).setDocumentFilter(new NumberOnlyFilter());
 
         error1Label = new JLabel("Nazwa użytkownika musi mieć więcej niż 3 znaki.");
@@ -60,35 +60,37 @@ public class UserRegistrationScreen extends JFrame {
         error2Label.setVisible(false);
         add(error2Label);
 
-        error3Label = new JLabel("Wiek musi być liczbą z zakresu 1 do 150.");
-        error3Label.setFont(new Font("Arial", Font.PLAIN, 14));
-        error3Label.setForeground(Color.RED);
-        error3Label.setBounds(85, 450, 550, 30);
-        error3Label.setVisible(false); 
-        add(error3Label);
 
         error4Label = new JLabel("Wybierz twój poziom umiejętności gry Sudoku");
         error4Label.setFont(new Font("Arial", Font.PLAIN, 14));
         error4Label.setForeground(Color.RED);
-        error4Label.setBounds(70, 610, 550, 30);
+        error4Label.setBounds(70, 535, 550, 30);
         error4Label.setVisible(false); 
         add(error4Label);
 
 
-        addSkillLevelRadioButtons(510); 
+        addSkillLevelRadioButtons(430); 
 
 
-        addButton("Zarejestruj", 650);
+        addButton("Zarejestruj", 580);
+        addButton2("Zaloguj się", 640);
         
         setLocationRelativeTo(null); 
         setLayout(null); 
     }
+
     
     private void addSkillLevelRadioButtons(int y) {
         JRadioButton beginnerButton = new JRadioButton("Początkujący");
         JRadioButton intermediateButton = new JRadioButton("Średniozaawansowany");
         JRadioButton advancedButton = new JRadioButton("Zaawansowany");
         JRadioButton expertButton = new JRadioButton("Ekspert");
+
+        beginnerButton.setActionCommand("Początkujący");
+        intermediateButton.setActionCommand("Średniozaawansowany");
+        advancedButton.setActionCommand("Zaawansowany");
+        expertButton.setActionCommand("Ekspert");
+
     
         beginnerButton.setBounds(100, y, 200, 20);
         intermediateButton.setBounds(100, y + 25, 300, 20);
@@ -213,42 +215,93 @@ public class UserRegistrationScreen extends JFrame {
         button.addActionListener(e -> {
             String username = usernameField.getText().trim();
             String pin = pinField.getText().trim();
-            String age = ageField.getText().trim();
-
-
+            String skillLevel = null;
+        
+            if (skillLevelGroup.getSelection() != null) {
+                skillLevel = skillLevelGroup.getSelection().getActionCommand();
+            }
+        
+            boolean valid = true;
+        
             if (username.length() <= 3) {
-                error1Label.setVisible(true); 
+                error1Label.setVisible(true);
+                valid = false;
             } else {
-                error1Label.setVisible(false); 
-
+                error1Label.setVisible(false);
             }
-
+        
             if (!pin.matches("\\d{4}")) {
-                error2Label.setVisible(true); 
+                error2Label.setVisible(true);
+                valid = false;
             } else {
-                error2Label.setVisible(false); 
-
+                error2Label.setVisible(false);
             }
-
-            if (!age.matches("\\d{1,3}") || Integer.parseInt(age) < 1 || Integer.parseInt(age) > 150) {
-                error3Label.setVisible(true);
-    
-            } else {
-                error3Label.setVisible(false);
-            }
-
-            if (skillLevelGroup.getSelection() == null) {
+        
+        
+            if (skillLevel == null) {
                 error4Label.setVisible(true);
+                valid = false;
             } else {
                 error4Label.setVisible(false);
             }
+        
+            if (valid) {
+                saveToJSON(username, pin, skillLevel);
+            }
         });
+        
+
+        
+
+        add(button);
+    }
+    private void addButton2(String text, int yPosition) {
+        JButton button = new JButton(text);
+        button.setBounds(90, yPosition, 250, 50);
+        button.setFont(new Font("Arial", Font.BOLD, 20));
+        button.setFocusPainted(false);
+        button.setForeground(ColorPalette.TEXT_DARK_GREEN); 
+        button.setBackground(ColorPalette.BUTTON_HIGHLIGHT_COLOR); 
+        button.setBorder(BorderFactory.createEmptyBorder());
+
+        button.setContentAreaFilled(false);
+        button.setOpaque(false);
+        button.setUI(new RoundedButtonUI());
+
+        // Podświetlenie przy kliknięciu
+        button.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mousePressed(MouseEvent e) {
+                button.setBackground(ColorPalette.TEXT_LIGHT_GREEN);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                button.setBackground(ColorPalette.BUTTON_HIGHLIGHT_COLOR);
+            }
+        });
+        
 
         
 
         add(button);
     }
 
+    private void saveToJSON(String username, String pin, String skillLevel) {
+        JSONObject userJson = new JSONObject();
+        userJson.put("username", username);
+        userJson.put("pin", pin);
+        userJson.put("skillLevel", skillLevel);
+    
+        try (FileWriter file = new FileWriter("registration_data.json")) {
+            file.write(userJson.toString(4)); 
+            file.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+            JOptionPane.showMessageDialog(this, "Błąd zapisu do pliku JSON.", "Błąd", JOptionPane.ERROR_MESSAGE);
+        }
+    }
+    
 
     private class NumberOnlyFilter extends DocumentFilter {
         @Override
@@ -318,7 +371,6 @@ public class UserRegistrationScreen extends JFrame {
 
             String text1 = "PIN:";
             String text2 = "Nazwa użytkownika:";
-            String text3 = "Wiek:";
             String text5 = "Twój poziom umiejętności gry:";
 
             g.setFont(new Font("Arial", Font.BOLD, 20));
@@ -326,8 +378,7 @@ public class UserRegistrationScreen extends JFrame {
 
             g.drawString(text2, 116, 250);
             g.drawString(text1, 190, 330);
-            g.drawString(text3, 185, 410);
-            g.drawString(text5, 70, 490);
+            g.drawString(text5, 70, 410);
         }
     }
     
