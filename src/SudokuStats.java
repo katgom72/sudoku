5ea2d01dd7b8d3e1bbcc96e779519afcb7488b22
katgom72 <kasiag11122001@gmail.com>
2024-12-19 13:52:46 +0100

    Remove duplicate classe

--- src/SudokuStats.java
+++ src/SudokuStats.java
@@ -309,21 +309,4 @@ public class SudokuStats extends JFrame {
 }
 
 
-class RoundedButtonUI extends javax.swing.plaf.basic.BasicButtonUI {
-    @Override
-    public void paint(Graphics g, JComponent c) {
-        JButton button = (JButton) c;
-        Graphics2D g2 = (Graphics2D) g;
-        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
 
-        g2.setColor(button.getBackground());
-        g2.fillRoundRect(0, 0, button.getWidth(), button.getHeight(), 30, 30);
-
-        FontMetrics fm = g2.getFontMetrics();
-        Rectangle r = new Rectangle(button.getWidth(), button.getHeight());
-        int x = (r.width - fm.stringWidth(button.getText())) / 2;
-        int y = (r.height - fm.getHeight()) / 2 + fm.getAscent();
-        g2.setColor(button.getForeground());
-        g2.drawString(button.getText(), x, y);
-    }
-}
